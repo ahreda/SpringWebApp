a9c8c448194ce19139cffd86b34bcab155f8645c
Ahmad Reda <ahmad.ah.reda@gmail.com>
2020-09-13 13:18:15 +0200

    First Commit

---
+++ src/test/java/com/ProjectI/SpringWebApp/SpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectI.SpringWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
